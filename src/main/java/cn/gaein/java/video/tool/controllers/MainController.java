d7dc1bb38df594b4b3ae8382b8d0fb79baa26599
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-06-25 13:00:28 +0800

    Add video player by VLC

--- src/main/java/cn/gaein/java/video/tool/controllers/MainController.java
+++ src/main/java/cn/gaein/java/video/tool/controllers/MainController.java
@@ -9,16 +9,24 @@ import com.jfoenix.controls.JFXListView;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
 import java.io.File;
 
+import static uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurfaceFactory.videoSurfaceForImageView;
+
 public class MainController {
+    private final MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+
+    private final EmbeddedMediaPlayer embeddedMediaPlayer;
+
     @FXML
     public JFXListView<InputVideoCell> inputFileList;
-
     @FXML
     public ImageView displayView;
-
     @FXML
     public JFXButton flagStartBtn;
     @FXML
@@ -34,8 +42,32 @@ public class MainController {
     @FXML
     public JFXButton addInputBtn;
 
+    public MainController() {
+        embeddedMediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
+        embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+            @Override
+            public void playing(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void paused(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void stopped(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
+            }
+        });
+    }
+
     @FXML
     protected void onAddInputClick() {
+        // TODO: move set video surface to init method
+        embeddedMediaPlayer.videoSurface().set(videoSurfaceForImageView(displayView));
+
         var inputFileListArr = inputFileList.getItems();
 
         var chooser = new FileChooser();
@@ -44,12 +76,21 @@ public class MainController {
         chooser.getExtensionFilters().addAll(FileExtensions.getVideoExtensions());
         var fileList = chooser.showOpenMultipleDialog(new Stage());
 
+        if (fileList == null)
+            return;
+
         fileList.forEach(file -> {
             var item = new InputVideoCell(new InputVideo(file));
+
             item.setOnDeleteClicked(e -> {
-                System.out.println("Remove");
+                // remove button clicked
                 inputFileListArr.remove(item);
             });
+            item.setOnMouseClicked(e -> {
+                // video item clicked
+                embeddedMediaPlayer.media().play(file.getPath());
+            });
+
             inputFileListArr.add(item);
         });
     }
