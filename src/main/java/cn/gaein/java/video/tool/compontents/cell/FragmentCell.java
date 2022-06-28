a5d9143f8897faa72e8dff34428f37c807903b6e
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-06-28 18:36:23 +0800

    Fix VideoFragmentCell wrong type

--- src/main/java/cn/gaein/java/video/tool/compontents/cell/FragmentCell.java
+++ src/main/java/cn/gaein/java/video/tool/compontents/cell/FragmentCell.java
@@ -1,6 +1,7 @@
 package cn.gaein.java.video.tool.compontents.cell;
 
 import cn.gaein.java.video.tool.models.Video;
+import cn.gaein.java.video.tool.models.VideoFragment;
 import io.github.palexdev.materialfx.controls.MFXListView;
 import io.github.palexdev.materialfx.controls.cell.MFXListCell;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -8,19 +9,19 @@ import org.kordamp.ikonli.javafx.FontIcon;
 /**
  * @author Gaein
  */
-public class FragmentCell extends MFXListCell<Video> {
+public class FragmentCell extends MFXListCell<VideoFragment> {
     private final FontIcon fragmentIcon
             = new FontIcon("mdi2m-movie-edit:18");
 
-    public FragmentCell(MFXListView<Video> listView, Video video) {
-        super(listView, video);
+    public FragmentCell(MFXListView<VideoFragment> listView, VideoFragment fragment) {
+        super(listView, fragment);
 
-        render(video);
+        render(fragment);
     }
 
     @Override
-    protected void render(Video data) {
-        super.render(data);
+    protected void render(VideoFragment fragment) {
+        super.render(fragment);
 
         // first render is null
         if (fragmentIcon != null) {
