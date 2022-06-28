99625fb294c8ba512b714a103ccde7a6441c6836
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-06-28 12:44:51 +0800

    Add author doc

--- src/main/java/cn/gaein/java/video/tool/MainApplication.java
+++ src/main/java/cn/gaein/java/video/tool/MainApplication.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * @author Gaein
+ */
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
