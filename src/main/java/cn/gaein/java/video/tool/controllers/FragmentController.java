b0886828bdc95a9d58b82d312b8811cfcbf22245
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-07-03 11:20:29 +0800

    Change to H264

--- src/main/java/cn/gaein/java/video/tool/controllers/FragmentController.java
+++ src/main/java/cn/gaein/java/video/tool/controllers/FragmentController.java
@@ -104,7 +104,7 @@ public class FragmentController implements Initializable {
 
         if (fragmentModel.enableEncodeProperty().get()) {
             fragment.edit(builder -> builder
-                    .addOption("-vcodec", "h265")
+                    .addOption("-vcodec", "h264")
                     .addOption("-acodec", "aac")
             );
         }
