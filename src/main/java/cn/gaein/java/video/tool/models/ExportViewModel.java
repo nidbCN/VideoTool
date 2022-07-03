1b9d859275576febf5fc3485a02bf02be9025b79
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-07-03 22:25:04 +0800

    Add codec properoty

--- src/main/java/cn/gaein/java/video/tool/models/ExportViewModel.java
+++ src/main/java/cn/gaein/java/video/tool/models/ExportViewModel.java
@@ -1,9 +1,6 @@
 package cn.gaein.java.video.tool.models;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.*;
 
 /**
  * @author Gaein
@@ -13,6 +10,10 @@ public class ExportViewModel {
             = new SimpleBooleanProperty();
     private final BooleanProperty enableEncode
             = new SimpleBooleanProperty();
+    private final StringProperty videoCodec
+            = new SimpleStringProperty();
+    private final StringProperty audioCodec
+            = new SimpleStringProperty();
     private final DoubleProperty videoRate
             = new SimpleDoubleProperty(2048);
     private final DoubleProperty audioRate
@@ -24,6 +25,14 @@ public class ExportViewModel {
 
     public BooleanProperty enableEncodeProperty() {
         return enableEncode;
+    }
+
+    public StringProperty videoCodecProperty() {
+        return videoCodec;
+    }
+
+    public StringProperty audioCodecProperty() {
+        return audioCodec;
     }
 
     public DoubleProperty videoRateProperty() {
